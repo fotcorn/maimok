a51d8beefd459e06b04c24fb76c8f9309ca17710
fotcorn <fotcorn@gmail.com>
2020-01-01 18:19:40 +0000

    frontend: nicer vm list

--- frontend/src/views/VMList.vue
+++ frontend/src/views/VMList.vue
@@ -1,5 +1,14 @@
 <template>
-  <v-data-table :headers="headers" :items="vms" :items-per-page="100" />
+  <v-data-table :headers="headers" :items="vms" :items-per-page="100">
+    <template v-slot:item.name="{ item }">
+      <v-icon small color="success" v-if="item.running">fa-play</v-icon>
+      <v-icon small color="default" v-else>fa-pause</v-icon>
+      <span class="ml-2">{{ item.name }}</span>
+    </template>
+    <template v-slot:item.memory="{ item }">
+      {{ formatMemory(item.memory) }} GB
+    </template>
+  </v-data-table>
 </template>
 
 <script lang="ts">
@@ -8,7 +17,6 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class VMList extends Vue {
   readonly headers = [
-    { text: "Running", value: "running" },
     { text: "Name", value: "name" },
     { text: "Memory", value: "memory" }
   ];
@@ -17,6 +25,10 @@ export default class VMList extends Vue {
   async mounted() {
     const response = await fetch("http://localhost:7000/vms");
     this.vms = await response.json();
+  }
+
+  formatMemory(memory: number) {
+    return +(memory / 1024 / 1024).toFixed(2);
   }
 }
 </script>
