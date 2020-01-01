4d585ca9ed94e46d4c67d2c3aca816c230604350
fotcorn <fotcorn@gmail.com>
2020-01-01 17:52:18 +0000

    backend: set CORS headers

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,7 @@ module github.com/fotcorn/go-kvm-web/backend
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-chi/cors v1.0.0
 	github.com/go-chi/render v1.0.1
 	github.com/libvirt/libvirt-go v5.8.0+incompatible
 	github.com/satori/go.uuid v1.2.0
--- backend/maimok/router.go
+++ backend/maimok/router.go
@@ -2,6 +2,7 @@ package maimok
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
 )
 
@@ -9,6 +10,11 @@ import (
 func GetRouter(state *globalState) chi.Router {
 	r := chi.NewRouter()
 	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	cors := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+	})
+	r.Use(cors.Handler)
 
 	r.Route("/vms", func(r chi.Router) {
 		r.Get("/", state.ListVMsHandler)
