482184acde102641fcb15f78f50bc1b6c0ab4473
fotcorn <fotcorn@gmail.com>
2020-01-01 17:59:37 +0000

    frontend: show vms

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@
     </v-app-bar>
 
     <v-content>
-      <v-container class="fill-height" fluid>
+      <v-container fluid>
         <router-view />
       </v-container>
     </v-content>
--- frontend/src/views/VMList.vue
+++ frontend/src/views/VMList.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>Hello World!</div>
+  <v-data-table :headers="headers" :items="vms" :items-per-page="100" />
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
-export default class VMList extends Vue {}
+export default class VMList extends Vue {
+  readonly headers = [
+    { text: "Running", value: "running" },
+    { text: "Name", value: "name" },
+    { text: "Memory", value: "memory" }
+  ];
+
+  vms = [];
+  async mounted() {
+    const response = await fetch("http://localhost:7000/vms");
+    this.vms = await response.json();
+  }
+}
 </script>
