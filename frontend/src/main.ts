65a37f9f9e4a9f015edfb2231e3cb8e4d4f6fc04
fotcorn <fotcorn@gmail.com>
2020-01-01 18:05:15 +0000

    frontend: add fontawesome, use local roboto font as configured from vuetify cli plugin

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,15 @@
-import "typeface-roboto/index.css";
-
 import Vue from "vue";
 
 import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 Vue.config.productionTip = false;
 
-new Vue({ router, vuetify, render: h => h(App) }).$mount("#app");
+new Vue({
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
